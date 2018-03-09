2c57c0aa9c956fc1930b768ed4410def42daed06
Ryan Lambert <ryan.g.lambert@gmail.com>
2018-03-09 09:50:05 -0800

    hide most of this stuff away to keep notebook clean

--- count_files.py
+++ count_files.py
@@ -1,21 +1,28 @@
+from IPython.display import display, HTML
 import os
+
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 skip_files = ['.DS_Store', 'README.txt', '__MACOSX']
 
 
-def make_file_description_data_frame():
+def make_file_description_data_frame(path='./ModelNet10/', show_missed=True):
     paths = []
-    for root, dirs, files in os.walk("./ModelNet10/"):
+    for root, dirs, files in os.walk(path):
         path = root.split(os.sep)
         for file in files:
             class_type = path[2]
             try:
                 # This could be better, but this isn't very important right now
+                # handling when the path terminates at a directory instead of a file
+                # So we're not acctually missing any files from being counted
                 sample_type = path[3]
             except:
-                print("woops!:")
-                print(path)
+                if show_missed:
+                    print("woops!:")
+                    print(path)
                 break
             paths.append({
                 'class': class_type,
@@ -23,3 +30,15 @@ def make_file_description_data_frame():
                 'file': file
             }) if not file in skip_files else None
     return pd.DataFrame(paths)
+
+
+def plot_class_balance(df, title):
+    grouped = df.groupby(['class', 'type'])['type'].count()
+    unstacked = grouped.unstack()
+    plt.figure()
+    unstacked = unstacked.sort_values(['test', 'train'])
+    unstacked.plot(kind='bar', title=title, figsize=(10, 6), yticks=np.arange(0, 900, 100))
+    plt.ylabel('Number of Samples')
+    plt.grid()
+    plt.show()
+    display(unstacked)
