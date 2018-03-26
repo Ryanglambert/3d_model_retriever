89163d4883868dba60fff9d508cf0c1ee7049443
Ryan Lambert <ryan.g.lambert@gmail.com>
2018-03-26 17:33:33 +0000

    more appropriate variable names

--- utils.py
+++ utils.py
@@ -81,25 +81,25 @@ def class_subset(x, y, class_name, class_name_list):
     return x[class_indices], y[class_indices], original_indices
 
 
-def query_corpus(latent_object, latent_corpus, query_size=10):
-    """Query a latent corpus (bottleneck features from NN)
+def query_latent_space(latent_object, latent_space, query_size=10):
+    """Query a latent space (bottleneck features from NN)
     latent_object : np.array
         object as represented in an N-D space
-    latent_corpus : np.array
+    latent_space : np.array
         matrix of N-d latent vectors each representing a model
-        in your associated corpus
+        in your associated latent space
     query_size : int
         number of relevant items to retrieve
 
     Returns
     -------
     top_n_sorted_sims : np.array
-        cosine similarities for the top n rows in latent_corpus
+        cosine similarities for the top n rows in latent_space
         where n is specified by `query_size`
     top_n_sorted_indices : np.array
-        indices of top n rows from the latent_corpus
+        indices of top n rows from the latent_space
     """
-    sims = latent_corpus.dot(latent_object.T)
+    sims = latent_space.dot(latent_object.T)
     sorted_sims_indices = np.argpartition(sims,
                                           range(-query_size, 0),
                                           axis=0)
